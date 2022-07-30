51f725ea4b45d46be1be0353b8cef15975326643
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-30 14:49:20 -0300

    create repo detail

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { FaArrowLeft } from 'react-icons/fa'
+
+import { Loading } from '../../components/Loading'
 import api from '../../libs/api'
+import { Container, Owner, GoBackButton } from './styles'
+
+interface Repo {
+  name: string
+  description: string
+  owner: {
+    name: string
+    avatarUrl: string
+  }
+}
 
 export function Repo() {
-  const [repo, setRepo] = useState({})
+  const [repo, setRepo] = useState<Repo>()
   const [issues, setIssues] = useState([])
 
   const params = useParams()
@@ -22,12 +35,50 @@ export function Repo() {
         }),
       ])
 
-      setRepo(repoData.data)
+      const repoSerialized: Repo = {
+        name: repoData.data.name,
+        description: repoData.data.description,
+        owner: {
+          avatarUrl: repoData.data.owner.avatar_url,
+          name: repoData.data.owner.login,
+        },
+      }
+
+      setRepo(repoSerialized)
       setIssues(issuesData.data)
     }
 
     getRepoData()
   }, [])
 
-  return <h1>Repo: {params?.repo}</h1>
+  if (!repo) {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Loading />
+      </div>
+    )
+  }
+
+  return (
+    <Container>
+      <GoBackButton to="/">
+        <FaArrowLeft size={20} />
+        Go Back
+      </GoBackButton>
+
+      <Owner>
+        <img src={repo.owner.avatarUrl} alt={repo.owner.name} />
+        <h1>{repo.name}</h1>
+        <p>{repo.description}</p>
+      </Owner>
+    </Container>
+  )
 }
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,5 +1,49 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export const Container = styled.div`
+  max-width: 700px;
+  background-color: #1f1f1f;
+  border-radius: 4px;
+  padding: 2rem;
+  margin: 5rem auto;
+`
 
-`;
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 8rem;
+    border-radius: 4px;
+  }
+
+  h1 {
+    font-size: 2rem;
+    line-height: 4rem;
+  }
+
+  p {
+    font-size: 0.875rem;
+    color: #9f9f9f;
+    max-width: 400px;
+    text-align: center;
+    line-height: 1.5rem;
+  }
+`
+
+export const GoBackButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  width: fit-content;
+  text-decoration: none;
+  color: #9f9f9f;
+  transition: color 0.1s ease-out;
+
+  &:hover {
+    color: #bbdefb;
+  }
+`
